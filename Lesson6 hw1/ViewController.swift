75ebd7587ddc51795bb06ab49a1331c5e99882f6
4er <vishnekidze@gmail.com>
2021-08-09 15:36:05 +0300

    Initial Commit

---
+++ Lesson6 hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson6 hw1
+//
+//  Created by Егор Вишняков on 9.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
